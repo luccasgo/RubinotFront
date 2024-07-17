2969387e44870b10767115e1ce8fc578efeddd2c
unknown <luccas-maciel@hotmail.com>
2024-07-17 12:37:37 -0300

    editar link do backend

--- src/app/guild.service.ts
+++ src/app/guild.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GuildService {
-  private apiUrl = 'http://localhost:3000/api/guild';
+  private apiUrl = 'https://rubinot-back.vercel.app/api/guild';
 
   constructor(private http: HttpClient) {}
 
